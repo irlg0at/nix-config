19099bd3a79c2ec71cb0fa328f6faafdb361ca1c
irlg0at <34748847+irlg0at@users.noreply.github.com>
2025-03-30 22:00:12 +0200

    Yambar configging

---
+++ modules/dwl/home.nix
@@ -0,0 +1,6 @@
+{pkgs,...}:
+{
+	imports = [
+		./programs/yambar.nix
+	];
+}
--- modules/dwl/programs/yambar.nix
+++ modules/dwl/programs/yambar.nix
@@ -1,12 +1,17 @@
-{pkgs,...}:
+{...}:
 {
-
-	environment.systemPackages = [
-		pkgs.yambar
-	];
-
-	programs.yambar.settings = {
-
-	};
-
+	programs.yambar = {
+		enable = true;
+		# Mostly fetched from https://codeberg.org/dnkl/yambar/src/branch/master/examples/configurations/laptop.conf
+		config = {
+			bar = {
+				  location =  "top";
+					height =  28;
+					background = "282a36ff";
+					margin = 10;
+					spacing = 8;
+					font = "Terminess Nerd Font Mono:style=Regular:pixelsize=22";
+					};
+			};
+		};
 }

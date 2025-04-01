3ee113d770c98951fc601aa49134060903aeeac9
irlg0at <34748847+irlg0at@users.noreply.github.com>
2025-04-01 15:54:01 +0200

    Fuzzel theming

--- modules/dwl/programs/fuzzel.nix
+++ modules/dwl/programs/fuzzel.nix
@@ -2,5 +2,19 @@
 {
 	programs.fuzzel = {
 		enable = true;
+		settings = {
+			main = {
+				font = "Terminess Nerd Font";
+			};
+			colors = {
+				background="282a36ff";
+				text="f8f8f2ff";
+				match="8be9fdff";
+				selection-match="8be9fdff";
+				selection="44475add";
+				selection-text="f8f8f2ff";
+				border="bd93f9ff";
+			};
+		};
 	};
 }

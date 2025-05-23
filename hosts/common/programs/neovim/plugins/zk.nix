a4db5d5abe82b5f535d19678c0c850820e08e53b
irlg0at <34748847+irlg0at@users.noreply.github.com>
2025-05-23 16:02:52 +0200

    Some keybinds for zk

--- hosts/common/programs/neovim/plugins/zk.nix
+++ hosts/common/programs/neovim/plugins/zk.nix
@@ -4,7 +4,31 @@
     plugins = {
       zk = {
         enable = true;
+				settings = {
+					picker = "telescope";
+				};
       };
     };
+
+		files = {
+			"ftplugin/markdown.lua" = {
+				keymaps = [
+					{
+						key = "<leader>zn";
+						mode = "n";
+						action = "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>";
+						options.desc = "Zk: New note";
+					}
+					{
+						key = "<leader>zl";
+						mode = "n";
+						action = "<Cmd>ZkInsertLink<CR>";
+						options.desc = "Zk: Insert link";
+					}
+				];
+			};
+
+		};
+
 	};
 }

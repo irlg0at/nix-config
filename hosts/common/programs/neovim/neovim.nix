7b25c54f50b10a8e8a13599a5b7431103fc29da7
irlg0at <34748847+irlg0at@users.noreply.github.com>
2025-06-13 17:16:27 +0200

    Powershell copy and paste for nvim

--- hosts/common/programs/neovim/neovim.nix
+++ hosts/common/programs/neovim/neovim.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs,lib, host, ... }:
 {
 
   imports = [
@@ -85,6 +85,21 @@
 			expandtab = true;
       syntax = "on";
     };
+
+    extraConfigVim = lib.mkIf (host == "wsl") ''
+      let g:clipboard = {
+        \   'name': 'WslClipboard',
+        \   'copy': {
+        \      '+': 'clip.exe',
+        \      '*': 'clip.exe',
+        \    },
+        \   'paste': {
+        \      '+': 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        \      '*': 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        \   },
+        \   'cache_enabled': 0,
+        \ }
+    '';
   };
 
 }

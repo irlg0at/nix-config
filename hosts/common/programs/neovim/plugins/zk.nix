70acf4e5a32434b4b61f6f28fa29ebf4cc8dab29
irlg0at <34748847+irlg0at@users.noreply.github.com>
2025-08-08 16:27:49 +0200

    More ZK nvim keybinds

--- hosts/common/programs/neovim/plugins/zk.nix
+++ hosts/common/programs/neovim/plugins/zk.nix
@@ -25,6 +25,30 @@
 						action = "<Cmd>ZkInsertLink<CR>";
 						options.desc = "Zk: Insert link";
 					}
+					{
+						key = "<leader>zN";
+						mode = "n";
+						action = "<Cmd>ZkNewFromTitleSelection<CR>";
+						options.desc = "Zk: New note from title selection";
+					}
+					{
+						key = "<leader>zf";
+						mode = "n";
+						action = "<Cmd>ZkNotes { sort = { 'modified' }, match = { vim.fn.input('Search: ') } }<CR>";
+						options.desc = "Zk: Find note based on content";
+					}
+					{
+						key = "<leader>zt";
+						mode = "n";
+						action = "<Cmd>ZkTags<CR>";
+						options.desc = "Zk: Filter by tag";
+					}
+					{
+						key = "<leader>zb";
+						mode = "n";
+						action = "<Cmd>ZkBacklinks<CR>";
+						options.desc = "Zk: Open notes linking to current buffer";
+					}
 				];
 			};
 

0603e9ee80b0baca3456359602eebc689c24f4aa
irlg0at <34748847+irlg0at@users.noreply.github.com>
2025-12-15 16:42:44 +0100

    ly: animation tweak, and pentest packages

--- modules/ly/ly.nix
+++ modules/ly/ly.nix
@@ -14,8 +14,8 @@
 			vi_mode = true;
 			waylandsessions = "/etc/ly/sessions/";
       animation = "colormix";
-      colormix_col1 = "0x${config.colorScheme.palette.base05}";
-      colormix_col2 = "0x${config.colorScheme.palette.base08}";
+      colormix_col1 = "0x${config.colorScheme.palette.base00}";
+      colormix_col2 = "0x${config.colorScheme.palette.base05}";
       colormix_col3 = "0x${config.colorScheme.palette.base09}";
 		};
 

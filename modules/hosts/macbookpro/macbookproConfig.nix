50876ddadae1c00b9d25227293e3eea40177d658
irlg0at <34748847+irlg0at@users.noreply.github.com>
2026-08-18 18:00:51 +0200

    kmonad: readd to specific macbookpro

--- modules/hosts/macbookpro/macbookproConfig.nix
+++ modules/hosts/macbookpro/macbookproConfig.nix
@@ -19,15 +19,16 @@
     ];
   };
 
-  # services.kmonad = {
-  # enable = true;
-  #    keyboards = {
-  #      myKMonadOutput = {
-  #        device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
-  #        config = builtins.readFile ./programs/kmonad/config.kbd;
-  #      };
-  #    };
-  # };
+
+  services.kmonad = {
+  enable = true;
+     keyboards = {
+       myKMonadOutput = {
+         device = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+         config = builtins.readFile ./kmonad/config.kbd;
+       };
+     };
+  };
 
 	# systemd.services."promisc@" = {
 	# 	description = "Set %i interface in promiscuous mode";

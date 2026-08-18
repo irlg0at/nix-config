0dfe2e2053621f133d100f6e376e1cb980f4a683
irlg0at <34748847+irlg0at@users.noreply.github.com>
2026-08-18 17:49:46 +0200

    niri: more keybinds, some styling

--- modules/programs/niri/default.nix
+++ modules/programs/niri/default.nix
@@ -12,12 +12,18 @@
       settings = {
         xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
         input.keyboard.xkb.layout = "no";
-        layout.gaps =16;
         spawn-at-startup = [
           (lib.getExe pkgs.waybar)
           (lib.getExe' pkgs.awww "awww-daemon")
           (lib.getExe pkgs.dunst)
         ];
+
+        layout = {
+          gaps = 16;
+          focus-ring.width = 3;
+        };
+
+        prefer-no-csd = _:{ };
 
         binds = {
           "Mod+Shift+Return".spawn-sh = lib.getExe pkgs.kitty;
@@ -42,6 +48,26 @@
           "Mod+Ctrl+J".move-window-down = _:{ };
           "Mod+Ctrl+K".move-window-up = _:{ };
           "Mod+Ctrl+L".move-column-right = _:{ };
+
+          # Switch Workspace
+          "Mod+1".focus-workspace = 1;
+          "Mod+2".focus-workspace = 2;
+          "Mod+3".focus-workspace = 3;
+          "Mod+4".focus-workspace = 4;
+          "Mod+5".focus-workspace = 5;
+          "Mod+6".focus-workspace = 6;
+          "Mod+7".focus-workspace = 7;
+          "Mod+8".focus-workspace = 8;
+          "Mod+9".focus-workspace = 9;
+          "Mod+Shift+1".move-column-to-workspace = 1;
+          "Mod+Shift+2".move-column-to-workspace = 2;
+          "Mod+Shift+3".move-column-to-workspace = 3;
+          "Mod+Shift+4".move-column-to-workspace = 4;
+          "Mod+Shift+5".move-column-to-workspace = 5;
+          "Mod+Shift+6".move-column-to-workspace = 6;
+          "Mod+Shift+7".move-column-to-workspace = 7;
+          "Mod+Shift+8".move-column-to-workspace = 8;
+          "Mod+Shift+9".move-column-to-workspace = 9;
         };
       };
     };

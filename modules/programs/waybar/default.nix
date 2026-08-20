b3b81eda064242900f8a881cd7969bec3f5b6168
g0at <34748847+irlg0at@users.noreply.github.com>
2026-08-20 16:48:21 +0200

    waybar: port themed style from main via base16 scheme
    Add the waybar CSS from main, mapping nix-colors palette references to
    the base16 scheme arg injected in perSystem.

--- modules/programs/waybar/default.nix
+++ modules/programs/waybar/default.nix
@@ -13,7 +13,7 @@
     ];
   });
   
-  perSystem = {pkgs, ...}: {
+  perSystem = {pkgs, scheme, ...}: {
     packages.waybar = inputs.wrapper-modules.wrappers.waybar.wrap {
       inherit pkgs;
       settings = {
@@ -22,6 +22,69 @@
           modules-right = ["battery" "cpu" "temperature" "backlight" "network" "clock"];
         };
       };
+      "style.css".content = ''
+        * {
+          font-family: Terminess Nerd Font;
+        }
+
+        #workspaces > * {
+          padding-left: 0.5rem;
+          margin-left: 0;
+          padding-right: 0.5rem;
+          margin-right: 0;
+        }
+
+        .modules-right > widget > * {
+          padding-left: 0.5rem;
+          margin-left: 0;
+          padding-right: 0.5rem;
+          margin-right: 0;
+          color: #${scheme.base00};
+        }
+
+        window#waybar {
+          background: #${scheme.base00};
+        }
+
+        #workspaces button {
+          color: #${scheme.base01};
+          border-radius: 0;
+          padding: 0;
+        }
+
+        #workspaces button.active {
+          color: #${scheme.base05};
+        }
+
+        #workspaces button.focused {
+          background-color: #${scheme.base09};
+          color: #${scheme.base00};
+        }
+
+        #battery {
+          background-color: #${scheme.base0A};
+        }
+
+        #cpu {
+          background-color: #${scheme.base0B};
+        }
+
+        #backlight {
+          background-color: #${scheme.base0C};
+        }
+
+        #temperature {
+          background-color: #${scheme.base0D};
+        }
+
+        #network {
+          background-color: #${scheme.base0E};
+        }
+
+        #clock {
+          background-color: #${scheme.base0F};
+        }
+      '';
     };
   };
 }

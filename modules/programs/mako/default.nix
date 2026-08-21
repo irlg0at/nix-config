6264b4e3ac42918225cab8798f596d3c1e23f6a0
g0at <34748847+irlg0at@users.noreply.github.com>
2026-08-21 16:12:34 +0200

    mako: smaller notifications

--- modules/programs/mako/default.nix
+++ modules/programs/mako/default.nix
@@ -18,14 +18,17 @@
       inherit pkgs;
       env.FONTCONFIG_FILE = fonts.fontconfigFile;
       settings = with scheme.withHashtag; {
-        font = "Terminess Nerd Font 11";
+        font = "Terminess Nerd Font 9";
         background-color = base00;
         text-color = base05;
         border-color = base0D;
-        border-size = 3;
-        border-radius = 8;
-        margin = 16;
-        padding = 12;
+        border-size = 2;
+        border-radius = 6;
+        width = 260;
+        height = 80;
+        margin = 10;
+        padding = 8;
+        max-icon-size = 32;
         default-timeout = 5000;
         "urgency=high" = {
           border-color = base08;

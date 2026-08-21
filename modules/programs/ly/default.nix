658efe07afba6cfcc85f789765571f5879d2cf21
g0at <34748847+irlg0at@users.noreply.github.com>
2026-08-21 15:53:55 +0200

    ly: theme with base16 colors and colormix animation

--- modules/programs/ly/default.nix
+++ modules/programs/ly/default.nix
@@ -1,20 +1,30 @@
-{...}:
+{ moduleWithSystem, ... }:
 {
-  flake.nixosModules.macbookproConfig = {pkgs, ...}: {
+  flake.nixosModules.macbookproConfig = moduleWithSystem ({ scheme, ... }: { pkgs, ... }:
+  let
+    hex = c: "0x00${c}";
+  in {
     environment.systemPackages = [
       pkgs.ly
     ];
-    
+
     services.displayManager.ly = {
       enable = true;
 
-      settings = {
+      settings = with scheme; {
         vi_mode = true;
         animation = "colormix";
+
+        bg = hex base00;
+        fg = hex base05;
+        border_fg = hex base0D;
+        error_bg = hex base00;
+        error_fg = hex base08;
+
+        colormix_col1 = hex base0D;
+        colormix_col2 = hex base0E;
+        colormix_col3 = hex base00;
       };
     };
-  };
+  });
 }
-
-
-

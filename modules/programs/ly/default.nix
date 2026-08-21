e957e78e03f60605fe6dda51919c174d533ce182
g0at <34748847+irlg0at@users.noreply.github.com>
2026-08-21 16:09:58 +0200

    ly: use saturated colors for the 16-color console

--- modules/programs/ly/default.nix
+++ modules/programs/ly/default.nix
@@ -14,16 +14,17 @@
       settings = with scheme; {
         vi_mode = true;
         animation = "colormix";
+        full_color = true;
 
         bg = hex base00;
         fg = hex base05;
-        border_fg = hex base0D;
+        border_fg = "0x001E90FF";
         error_bg = hex base00;
-        error_fg = hex base08;
+        error_fg = "0x00FF0000";
 
-        colormix_col1 = hex base0D;
-        colormix_col2 = hex base0E;
-        colormix_col3 = hex base00;
+        colormix_col1 = "0x000000FF";
+        colormix_col2 = "0x00FF00FF";
+        colormix_col3 = "0x0000FFFF";
       };
     };
   });

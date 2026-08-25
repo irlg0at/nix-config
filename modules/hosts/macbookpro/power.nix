1b206a1ca26975819c3da9b51ee9cb4afab34e79
g0at <34748847+irlg0at@users.noreply.github.com>
2026-08-25 14:32:32 +0200

    macbookpro: battery optimization (powertop, upower, 80% charge limit)

---
+++ modules/hosts/macbookpro/power.nix
@@ -0,0 +1,21 @@
+{ ... }:
+{
+  flake.nixosModules.macbookproConfig = { pkgs, ... }: {
+    powerManagement.powertop.enable = true;
+    services.upower.enable = true;
+
+    systemd.services.battery-charge-limit = {
+      description = "Limit battery charge to 80%";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = pkgs.writeShellScript "battery-charge-limit" ''
+          f=/sys/class/power_supply/BAT1/charge_control_end_threshold
+          if [ -w "$f" ]; then echo 80 > "$f"; fi
+        '';
+      };
+    };
+  };
+}

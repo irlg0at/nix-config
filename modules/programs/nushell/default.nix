1b339b70fa29a10ab7644d4d6157ae1a8d73d062
g0at <34748847+irlg0at@users.noreply.github.com>
2026-08-31 17:52:40 +0200

    nushell: add direnv

--- modules/programs/nushell/default.nix
+++ modules/programs/nushell/default.nix
@@ -26,17 +26,25 @@
       export HOME=$TMPDIR
       ${pkgs.starship}/bin/starship preset pure-preset > $out
     '';
+    direnvConfig = pkgs.writeTextDir "direnvrc"
+      "source ${pkgs.nix-direnv}/share/nix-direnv/direnvrc";
   in {
     packages.nushell = inputs.wrapper-modules.wrappers.nushell.wrap {
       inherit pkgs;
-      prefixVar = [ { name = "shellDeps"; data = [ "PATH" ":" "${pkgs.lib.makeBinPath [ pkgs.zoxide pkgs.starship ]}" ]; } ];
+      prefixVar = [ { name = "shellDeps"; data = [ "PATH" ":" "${pkgs.lib.makeBinPath [ pkgs.zoxide pkgs.starship pkgs.direnv ]}" ]; } ];
       env.STARSHIP_CONFIG = starshipConfig;
       env.BAT_THEME = "base16-256";
+      env.DIRENV_CONFIG = direnvConfig;
       "config.nu".content = ''
         $env.config.show_banner = false
         source ${zoxideInit}
         use ${starshipInit}
         alias cat = ${pkgs.lib.getExe pkgs.bat} --paging=never
+        $env.config.hooks.pre_prompt = ($env.config.hooks.pre_prompt | append {||
+          let vars = (direnv export json | from json | default {})
+          $vars | load-env
+          if "PATH" in $vars { $env.PATH = ($vars.PATH | split row (char esep)) }
+        })
       '';
     };
   };
